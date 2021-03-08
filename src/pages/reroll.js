840b9541a6c659ccf123de3b26bc6e6e8658f8a5
Brett Bejcek <brett@Bretts-Macbook.local>
2021-03-07 20:02:17 -1000

    added disabled button and catch if no internet

--- src/pages/reroll.js
+++ src/pages/reroll.js
@@ -5,38 +5,54 @@ import { trackCustomEvent } from "gatsby-plugin-google-analytics"
 export default class Signup extends PureComponent {
 
   state = {
-    "challenge":"Getting a different challenge for you...please wait 5 seconds",
+    "challenge":"Getting a challenge for you...please wait 5 seconds",
     "fetching":true
   }
 
   componentDidMount() {
+    try{
     fetch("https://us-central1-create-more.cloudfunctions.net/get-random-challenge")
     .then( (response) => {
               return response.json() })
                   .then( (json) => {
                       this.setState({challenge: json.challenge, fetching: false})
                   })
+    }
+    catch{
+      this.setState({challenge:"Oops, there was trouble fetching, please try again later.", fetching:false})
+    }
+
  }
 
   reRoll = event => {
     this.setState({challenge: "Getting a different challenge for you...", fetching:true})
+
+    try{
+    fetch("https://us-central1-create-more.cloudfunctions.net/get-random-challenge")
+    .then( (response) => {
+              return response.json() })
+                  .then( (json) => {
+                      this.setState({challenge: json.challenge, fetching: false})
+                  })
     trackCustomEvent({
           category: "Reroll",
           action: "Success",
         })
-    fetch("https://us-central1-create-more.cloudfunctions.net/get-random-challenge")
-        .then( (response) => {
-              return response.json() })
-                  .then( (json) => {
-                      this.setState({challenge: json.challenge, fetching:false})
-                  })
+    }
+    catch{
+      this.setState({challenge:"Oops, there was trouble fetching, please try again later.", fetching:false})
+          trackCustomEvent({
+          category: "Reroll",
+          action: "Fail",
+        })
+    }
   }
   render()
   {
     return (
     <div style={{textAlign:`center`}}>
     <p style={{fontFamily:`Poppins`,color:`white`, paddingLeft:`30px`, paddingRight:`30px`, fontSize:`x-large`}}>{this.state.challenge}</p>
-<button disabled={this.state.fetching} onClick={this.reRoll}style={{fontFamily:`Poppins`,color:`white`, background:`black`, border:`white solid 3px`}}>Reroll</button>
+    <button disabled={this.state.fetching} onClick={this.reRoll}style={{fontFamily:`Poppins`,color:`white`, background:`black`, border:`white solid 3px`}}>Reroll</button>
 
   </div>
 
